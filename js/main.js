2f52a8582c029db95d5debb5e7c23358fcde6eca
Anthony Sherwood <iworkinpixels@gmail.com>
2013-07-09 18:00:40 -0400

    Multi Page Framework
    Adds new controls and placeholders for multi-page.  Everything is
    working now, so we just need to add modes for the other pages and
    do some content.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var mode="answers";
+
 $(document).ready(function() {
   $(document).keydown(function(e){
     switch(e.which) {
@@ -7,35 +9,63 @@ $(document).ready(function() {
         break;
          
       case 37: // left is select previous answer
-          if ($('#li-d').hasClass('selected-answer'))
-            switchTo('#li-c');
-          else if ($('#li-c').hasClass('selected-answer'))
-            switchTo('#li-b');
-          else if ($('#li-b').hasClass('selected-answer'))
-            switchTo('#li-a');
-          else if ($('#li-a').hasClass('selected-answer'))
-            switchTo('#li-d');
-          else
-            switchTo('#li-d');
+          if (mode == 'answers') {
+            if ($('#li-d').hasClass('selected-answer'))
+              switchTo('#li-c');
+            else if ($('#li-c').hasClass('selected-answer'))
+              switchTo('#li-b');
+            else if ($('#li-b').hasClass('selected-answer'))
+              switchTo('#li-a');
+            else if ($('#li-a').hasClass('selected-answer'))
+              switchTo('#li-d');
+            else
+              switchTo('#li-d');
+          } else {
+            if ($('#nav-home').hasClass('selected'))
+              rotateNav('#nav-tests');
+            else if ($('#nav-tests').hasClass('selected'))
+              rotateNav('#nav-weather');
+            else if ($('#nav-weather').hasClass('selected'))
+              rotateNav('#nav-news');
+            else if ($('#nav-news').hasClass('selected'))
+              rotateNav('#nav-home');
+          }
           break;
             
       case 39: // right is select next answer
-          if ($('#li-a').hasClass('selected-answer'))
-            switchTo('#li-b');
-          else if ($('#li-b').hasClass('selected-answer'))
-            switchTo('#li-c');
-          else if ($('#li-c').hasClass('selected-answer'))
-            switchTo('#li-d');
-          else if ($('#li-d').hasClass('selected-answer'))
-            switchTo('#li-a');
-          else
-            switchTo('#li-a');
+          if (mode == 'answers') {
+            if ($('#li-a').hasClass('selected-answer'))
+              switchTo('#li-b');
+            else if ($('#li-b').hasClass('selected-answer'))
+              switchTo('#li-c');
+            else if ($('#li-c').hasClass('selected-answer'))
+              switchTo('#li-d');
+            else if ($('#li-d').hasClass('selected-answer'))
+              switchTo('#li-a');
+            else
+              switchTo('#li-a');
+          } else {
+            if ($('#nav-home').hasClass('selected'))
+              rotateNav('#nav-news');
+            else if ($('#nav-news').hasClass('selected'))
+              rotateNav('#nav-weather');
+            else if ($('#nav-weather').hasClass('selected'))
+              rotateNav('#nav-tests');
+            else if ($('#nav-tests').hasClass('selected'))
+              rotateNav('#nav-home');
+          }
           break;
       
       case 38: // up is page up
-          return;
+        mode = "nav";
+        $('#menubar-coverup').css('display','none');
+        $('#main-div-coverup').css('display','block');
+        return;
       case 40: // down is page down
-          return;
+        mode = "answers";
+        $('#menubar-coverup').css('display','block');
+        $('#main-div-coverup').css('display','none');
+        return;
       default: return; // exit this handler for other keys
     }
     e.preventDefault();
@@ -72,5 +102,32 @@ function checkTime(i) {
 function switchTo(theID){
   $('li.selected-answer').removeClass('selected-answer');
   $(theID).addClass('selected-answer');
+}
+
+function rotateNav(theID){
+  $('#nav li.selected').removeClass('selected');
+  $(theID).addClass('selected');
+  // Turn off all the content
+  $('#home').css('display', 'none');
+  $('#news').css('display', 'none');
+  $('#weather').css('display', 'none');
+  $('#tests').css('display', 'none');
+  // Bring back the selected one
+  switch(theID) {
+    case "#nav-home":
+      $('#home').css('display', 'block');
+      break;
+    case "#nav-news":
+      $('#news').css('display', 'block');
+      break;
+    case "#nav-weather":
+      $('#weather').css('display', 'block');
+      break;
+    case "#nav-tests":
+      $('#tests').css('display', 'block');
+      break;
+
+  }
+
 }
 
